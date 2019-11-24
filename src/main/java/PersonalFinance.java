f1cd91e7c0a48218913a8511349f45250751efb9
Siamen Maskalenchyk <siamenmaskalenchyk@gmail.com>
2019-11-24 23:41:36 +0300

    add Text class

--- src/main/java/PersonalFinance.java
+++ src/main/java/PersonalFinance.java
@@ -1,6 +1,23 @@
+import settings.Text;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 public class PersonalFinance {
 
     public static void main(String[] args) {
+    init();
+        System.out.println(Text.get("PROGRAM_NAME"));
+    }
 
+    private static void init() {
+        Text.init();
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/Kornilow.ttf")));
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/settings/Text.java
+++ src/main/java/settings/Text.java
@@ -4,11 +4,49 @@ import java.util.HashMap;
 
 final public class Text {
 
-    private static final HashMap <String, String> data = new HashMap<String, String>();
+    private static final HashMap<String, String> data = new HashMap<String, String>();
 
-    public static void init(){
+    public static String get(String key) {
+        return data.get(key);
+    }
+
+    public static String[] getMonth() {
+        String[] months = new String[12];
+        months[0] = data.get("JANUARY");
+        months[1] = data.get("FEBRUARY");
+        months[2] = data.get("MARCH");
+        months[3] = data.get("APRIL");
+        months[4] = data.get("MAY");
+        months[5] = data.get("JUNE");
+        months[6] = data.get("JULY");
+        months[7] = data.get("AUGUST");
+        months[8] = data.get("SEPTEMBER");
+        months[9] = data.get("OCTOBER");
+        months[10] = data.get("NOVEMBER");
+        months[11] = data.get("DECEMBER");
+        return months;
+    }
+
+    public static void init() {
         data.put("PROGRAM_NAME", "Домашняя бухгалтерия");
         data.put("MENU_FILE", "Файл");
+        data.put("MENU_EDIT", "Справка");
+        data.put("MENU_VIEW", "Вид");
+        data.put("MENU_HELP", "Помощь");
+
+        data.put("JANUARY", "Январь");
+        data.put("FEBRUARY", "Февраль");
+        data.put("MARCH", "Март");
+        data.put("APRIL", "Апрель");
+        data.put("MAY", "Май");
+        data.put("JUNE", "Июнь");
+        data.put("JULY", "Июль");
+        data.put("AUGUST", "Август");
+        data.put("SEPTEMBER", "Сентябрь");
+        data.put("OCTOBER", "Октябрь");
+        data.put("NOVEMBER", "Ноябрь");
+        data.put("DECEMBER", "Декабрь");
 
     }
+
 }
