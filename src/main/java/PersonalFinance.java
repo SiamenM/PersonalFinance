e6e3428b9431ed696d44651ba2daa3c54d30b90e
Cex 4 <cex4agat@mail.ru>
2019-11-28 15:34:18 +0300

    add settings class

--- src/main/java/PersonalFinance.java
+++ src/main/java/PersonalFinance.java
@@ -1,21 +1,22 @@
+import settings.Settings;
 import settings.Text;
 
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
 
 public class PersonalFinance {
 
     public static void main(String[] args) {
-    init();
+        init();
         System.out.println(Text.get("PROGRAM_NAME"));
     }
 
     private static void init() {
+        Settings.init();
         Text.init();
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         try {
-            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/fonts/Kornilow.ttf")));
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, Settings.FONT_KORNILOW));
         } catch (FontFormatException | IOException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/settings/Settings.java
@@ -0,0 +1,69 @@
+package settings;
+
+import org.ini4j.Ini;
+import org.ini4j.IniPreferences;
+import org.ini4j.Wini;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.prefs.Preferences;
+
+public class Settings {
+
+    public static final File FONT_KORNILOW = new File("src/fonts/Kornilow.ttf");
+    public static final File SAVE_DIR = new File("saves/");
+    public static final String SAVE_FILE_EXT = "myfin";
+
+    private static final String FORMAT_AMOUNT = "%.2f";
+    private static final String FORMAT_RATE = "%.4f";
+    private static final String FORMAT_DATE = "dd.MM.yyyy";
+    private static final String FORMAT_DATE_MONTH = "MMMM yyyy";
+    private static final String FORMAT_DATE_YEAR = "yyyy";
+
+    public static final int COUNT_OVERVIEW_ROWS = 10;
+
+    public static final String[] CURRENCIES_CODES = {"RUB", "USD", "EUR", "BYN", "UAH"};
+
+    private static final File FILE_SETTINGS = new File("src/saves/settings.ini");
+    private static File fileSave = new File("src/saves/settings.ini");
+
+    public static void init() {
+        try {
+            Ini ini = new Ini(FILE_SETTINGS);
+            Preferences prefs = new IniPreferences(ini);
+            String s = prefs.node("Settings").get("fileSave", null);
+            if (s != null) {
+                fileSave = new File(s);
+            }
+            setLocale();
+        } catch (IOException e) {
+            save();
+        }
+
+    }
+
+    private static void setLocale() {
+        Locale.setDefault(new Locale("ru"));
+    }
+
+    public static File getFileSave() {
+        return fileSave;
+    }
+
+    public static void setFileSave(File fileSave) {
+        Settings.fileSave = fileSave;
+        save();
+    }
+
+    private static void save() {
+        Wini ini = null;
+        try {
+            ini = new Wini(FILE_SETTINGS);
+            ini.put("Settings", "fileSave", fileSave.getAbsolutePath().replace("\\", "\\\\"));
+            ini.store();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
