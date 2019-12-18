a246f2e46905d800a6818693d0c033071b3e9540
Cex 4 <cex4agat@mail.ru>
2019-12-18 09:05:36 +0300

    add window about

--- src/main/code/UI/Controller.java
+++ src/main/code/UI/Controller.java
@@ -2,6 +2,15 @@ package UI;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import settings.Text;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Controller {
 
@@ -9,8 +18,15 @@ public class Controller {
         Platform.exit();
     }
 
-    public void pressAbout(ActionEvent about){
-
+    public void pressAbout(ActionEvent about) throws IOException {
+        Parent aboutRoot = FXMLLoader.load(getClass().getResource("/UI/About.fxml"));
+        Stage aboutStage = new Stage();
+        aboutStage.setScene(new Scene(aboutRoot));
+        aboutStage.setTitle(Text.get("MENU_HELP_ABOUT"));
+        aboutStage.setResizable(false);
+        Image iconMain = new Image(new File("images/about.png").toURI().toString());
+        aboutStage.getIcons().add(iconMain);
+        aboutStage.show();
     }
     public void press(ActionEvent event) {
 
