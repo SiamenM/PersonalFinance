acbbce83cdeed6148d6916baad86cdedeafb948b
Cex 4 <cex4agat@mail.ru>
2020-02-04 15:35:14 +0300

    optimize filter panel

--- src/main/code/UI/FilterPanel/FilterPanel.java
+++ src/main/code/UI/FilterPanel/FilterPanel.java
@@ -5,9 +5,9 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
 import saveLoad.SaveData;
 import settings.Format;
-
 
 public class FilterPanel extends HBox {
 
@@ -22,13 +22,8 @@ public class FilterPanel extends HBox {
     private void initFilterPanel() {
         Button left = new Button("", new ImageView("/images/left.png"));
         Button step = new Button(Format.getTitleFilter(SaveData.getInstance().getFilter()));
+        step.setFont(Font.font("Verdana", 14));
         Button right = new Button("", new ImageView("/images/right.png"));
-        left.setMaxHeight(20);
-        left.setPrefHeight(20);
-        right.setMaxHeight(20);
-        step.setMinWidth(50);
-        step.setMaxHeight(left.getMaxHeight());
-        step.setPrefHeight(left.getMaxHeight());
         this.getChildren().addAll(left, step, right);
     }
 }
